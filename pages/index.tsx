f128962a758347c6fc2349f3dba86318adb3cd2d
Yaks Souza <pl4nk1ton@gmail.com>
2022-02-15 17:59:36 -0300

    Fix integration and add background style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,31 @@
 import { ThemeProvider } from 'styled-components'
+import { useState, useEffect } from 'react'
 
 import * as S from 'styles/index'
 import { homeTheme } from "styles/themes"
 
 export default function Home() {
 	const [message, setMsg] = useState('Hello, world!');
-	
+	const [name, setName] = useState('world');
+
+
 	useEffect(() => {
-		fetch("https://baldedelixo-api.herokuapp.com").
-		then(r => r.text()).
-		then(msg => {
-			setMsg(msg);
-		});
-	}, []);
+		fetch(`https://baldedelixo-api.herokuapp.com/?name=${name}`).
+		then(r => r.json()).
+		then(data => {
+			setMsg(data.message)
+		}).
+    catch(e => {
+        console.log(e)
+      });
+	}, [name]);
+
 	return (
-		<ThemeProvider theme={homeTheme}>
-			<S.Title>{message}</S.Title>
+    <ThemeProvider theme={homeTheme}>
+      <S.Background>
+        <input placeholder="Type your name" type="text" onChange={e => setName(e.target.value)}/>
+        <S.Title>{message}</S.Title>
+      </S.Background>
 		</ThemeProvider>
 	)
 }
--- styles/index.ts
+++ styles/index.ts
@@ -3,3 +3,10 @@ import styled from "styled-components"
 export const Title = styled.h1`
 	color: ${({ theme }) => theme.colors.primary};
 `
+
+export const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`
