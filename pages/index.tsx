c731883b98dcf11209f986a52e28e02acfb04086
Yaks Souza <pl4nk1ton@gmail.com>
2022-02-15 17:36:03 -0300

    Fetch message of actual backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,18 @@ import * as S from 'styles/index'
 import { homeTheme } from "styles/themes"
 
 export default function Home() {
+	const [message, setMsg] = useState('Hello, world!');
+	
+	useEffect(() => {
+		fetch("https://baldedelixo-api.herokuapp.com").
+		then(r => r.text()).
+		then(msg => {
+			setMsg(msg);
+		});
+	}, []);
 	return (
 		<ThemeProvider theme={homeTheme}>
-			<S.Title>Olá, mundo!</S.Title>
+			<S.Title>{message}</S.Title>
 		</ThemeProvider>
 	)
 }
